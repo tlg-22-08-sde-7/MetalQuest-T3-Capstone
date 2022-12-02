6db063d6972c6fd52ca9df23280fc8ec05823751
jennytoc <toctocanj@gmail.com>
2022-12-02 16:25:15 -0700

    Added class Commands and implemented GSOn AB#19

--- src/com/metalquest/Commands.java
+++ src/com/metalquest/Commands.java
@@ -1,7 +1,29 @@
 package com.metalquest;
 
-public class Commands {
-    public static void showCommands() {
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Commands {
+    public static void main(String[] args) {
+//        showCommands();
+    }
+    public static void showCommands(String location) {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("resources/commands.json"));
+            JsonObject parser = JsonParser.parseReader(reader).getAsJsonObject();
+
+            for (JsonElement command : parser.get("commands").getAsJsonArray()) {
+                JsonObject obj = command.getAsJsonObject();
+                System.out.println(obj.get("keyword").getAsString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
