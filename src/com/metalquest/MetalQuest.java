0690990a99128dbe585a3e1e2c8d16e8e4e97a9a
jennytoc <toctocanj@gmail.com>
2022-12-02 14:22:39 -0700

    Added objective method to class MetalQuest AB#14

--- src/com/metalquest/MetalQuest.java
+++ src/com/metalquest/MetalQuest.java
@@ -1,9 +1,11 @@
 package com.metalquest;
 
 import static com.metalquest.SplashScreen.splashScreen;
+import static com.metalquest.Objective.objectiveMsg;
 
 class MetalQuest {
     public static void main(String[] args) {
         splashScreen();
+        objectiveMsg();
     }
 }
