f093582d1f59527d436274af4476a1e919f5ab38
Dorian Majano <dorian.majano@gmail.com>
2022-12-02 13:12:41 -0800

    Add ASCII Art code block AB#13

--- src/com/metalquest/SplashScreen.java
+++ src/com/metalquest/SplashScreen.java
@@ -3,5 +3,9 @@ package com.metalquest;
 class SplashScreen {
     public static void splashScreen() {
         System.out.println("Welcome to Metal Quest");
+//        try (BufferedReader br = new BufferedReader(new FileReader("images/banner.txt"))) {
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
     }
 }
