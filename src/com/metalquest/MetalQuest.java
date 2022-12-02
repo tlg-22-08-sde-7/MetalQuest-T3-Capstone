bb235d37c009df066f3fea072e52533c5faf4abd
jennytoc <toctocanj@gmail.com>
2022-12-02 14:27:46 -0700

    Fixing merge conflict AB#14

--- src/com/metalquest/MetalQuest.java
+++ src/com/metalquest/MetalQuest.java
@@ -1,11 +1,13 @@
 package com.metalquest;
 
+//import static com.metalquest.NewGame.newGame;
 import static com.metalquest.SplashScreen.splashScreen;
 import static com.metalquest.Objective.objectiveMsg;
 
 class MetalQuest {
     public static void main(String[] args) {
-        splashScreen();
+        splashScreen();    //TODO: update splash screen to correct name - Mark's todo
+//        newGame();
         objectiveMsg();
     }
 }
