65c7713cdea44b98d83258e286a4d9ea7feffe74
Mark <marksomething@gmail.com>
2022-12-02 15:19:23 -0600

    ASCII art rough draft with need to correct name

--- src/com/metalquest/SplashScreen.java
+++ src/com/metalquest/SplashScreen.java
@@ -1,7 +1,19 @@
 package com.metalquest;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 class SplashScreen {
     public static void splashScreen() {
+        try (BufferedReader br = new BufferedReader(new FileReader("images/banner.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
