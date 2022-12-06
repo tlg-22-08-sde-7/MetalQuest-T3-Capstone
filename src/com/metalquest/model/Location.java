33771466e7c266f4a37fb1ae7d7d8bca77ade239
ibrahimdev23 <ibtheonemohamed13@gmail.com>
2022-12-06 16:52:22 -0600

    made changes to noun/verb method, and modified verbs.json file, AB#18

--- src/com/metalquest/model/Location.java
+++ src/com/metalquest/model/Location.java
@@ -1,47 +1,47 @@
 package com.metalquest.model;
 
 class Location {
-    private String location;
-    private Direction direction;
-    private String description;
-    private Item [] items;
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Item[] getItems() {
-        return items;
-    }
-
-    public void setItems(Item[] items) {
-        this.items = items;
-    }
-
-    @Override
-    public String toString() {
-        return "Hello World";
-    }
+//    private String location;
+//    private Direction direction;
+//    private String description;
+//    private Item [] items;
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
+//
+//    public Direction getDirection() {
+//        return direction;
+//    }
+//
+//    public void setDirection(Direction direction) {
+//        this.direction = direction;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public Item[] getItems() {
+//        return items;
+//    }
+//
+//    public void setItems(Item[] items) {
+//        this.items = items;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Hello World";
+//    }
 }
 
 
