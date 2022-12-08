4727ac8df036e35f3c0ca9e8e149089fe9cdd929
Dorian Majano <dorian.majano@gmail.com>
2022-12-08 08:10:28 -0800

    Push lookItem function AB#22

--- src/com/metalquest/controller/Game.java
+++ src/com/metalquest/controller/Game.java
@@ -156,6 +156,19 @@ public class Game {
 
     private String lookItem(String item) {
         String itemDescription = "";
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("json/items.json"));
+            JsonObject parser = JsonParser.parseReader(reader).getAsJsonObject();
+
+            for (JsonElement obj : parser.get("items").getAsJsonArray()) {
+                JsonObject itemName = obj.getAsJsonObject();
+                if (item.equals(itemName.get("name").getAsString())) {
+                    itemDescription = itemName.get("description").getAsString();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return itemDescription;
     }
 
