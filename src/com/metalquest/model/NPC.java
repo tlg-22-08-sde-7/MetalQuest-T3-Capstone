bc95a60d1b98372f3d44190aa5a141bcfa414a84
Dorian Majano <dorian.majano@gmail.com>
2022-12-08 14:47:02 -0800

    Add NPC class AB#37

---
+++ src/com/metalquest/model/NPC.java
@@ -0,0 +1,5 @@
+package com.metalquest.model;
+
+class NPC {
+
+}
--- src/com/metalquest/model/Player.java
+++ src/com/metalquest/model/Player.java
@@ -14,6 +14,7 @@ public class Player {
 //        private Player() {
 //
 //        }
+
     public String talk() {
         return "";
     }
