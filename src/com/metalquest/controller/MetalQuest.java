dc67fda55cf5f908cb1f10efb23c9096780c609d
Dorian Majano <dorian.majano@gmail.com>
2022-12-14 09:12:13 -0800

    Refactor MetalQuest AB#35

--- src/com/metalquest/controller/MetalQuest.java
+++ src/com/metalquest/controller/MetalQuest.java
@@ -1,7 +1,5 @@
 package com.metalquest.controller;
 
-import com.metalquest.controller.Game;
-
 public class MetalQuest {
     public static void main(String[] args) {
         Game game = new Game();
